d897f0a68688d196340a55cf3141536de8cc2a92
Han <156781329+HanQ233@users.noreply.github.com>
2025-03-19 10:20:36 +0800

    Add files via upload

--- QH_plt_rcParams.py
+++ QH_plt_rcParams.py
@@ -22,20 +22,20 @@ plt.rcParams.update({  # 用.update可以一次设置很多参数。
     'font.size': 10,
 #坐标轴
     "axes.linewidth": 0.5,
-    'axes.labelsize': 18,
-    "lines.linewidth": 0.5,
+    'axes.labelsize': 20,
+    "lines.linewidth": 0.5, #控制scatter描边粗细，太细显示的不好看
     "lines.markersize": 15,
     "lines.markeredgewidth": 0.5,
     "xtick.minor.visible": True,
     "ytick.minor.visible": True,
-    "xtick.major.size": 10,
-    "ytick.major.size": 10,
+    "xtick.major.size": 8,
+    "ytick.major.size": 8,
     "xtick.minor.size": 4,
     "ytick.minor.size": 4,
     "xtick.major.pad": 7,  # 间距
     "ytick.major.pad": 7,  # 间距
-    "xtick.labelsize": 18, 
-    "ytick.labelsize": 18, 
+    "xtick.labelsize": 20, 
+    "ytick.labelsize": 20, 
     "xtick.direction": 'in',
     "ytick.direction": 'in',
     "xtick.major.width": 0.5,
